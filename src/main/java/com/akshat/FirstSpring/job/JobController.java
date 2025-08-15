9427760fe95409111a4f5c91ed82f6c5a87a9812
AkshatRaiShrivastava <shrivastavak2005@gmail.com>
2025-08-15 11:41:16 +0530

    added response entity in job controller

--- src/main/java/com/akshat/FirstSpring/job/JobController.java
+++ src/main/java/com/akshat/FirstSpring/job/JobController.java
@@ -2,7 +2,10 @@ package com.akshat.FirstSpring.job;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,6 +27,15 @@ public class JobController {
     public String postJob(@RequestBody Job job) {
         jobService.createJob(job);
         return "Job Posted !!";
+    }
+
+    @GetMapping("/jobs/{id}")
+    public ResponseEntity<Job> getJobById(@PathVariable Long id) {
+        Job job = jobService.getJobById(id);
+        if (job != null) {
+            return new ResponseEntity<>(job, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
 }
--- src/main/java/com/akshat/FirstSpring/job/implementation/JobserviceImplementation.java
+++ src/main/java/com/akshat/FirstSpring/job/implementation/JobserviceImplementation.java
@@ -2,6 +2,7 @@ package com.akshat.FirstSpring.job.implementation;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 import org.springframework.stereotype.Service;
 
@@ -12,24 +13,35 @@ import com.akshat.FirstSpring.job.JobService;
 public class JobserviceImplementation implements JobService {
     private List<Job> jobs = new ArrayList<>();
     private Long nextId = 1L;
+
     @Override
     public List<Job> findAll() {
         return jobs;
     }
-    
+
     // public static void main(String[] args) {
-    //      List<Job> jobs = new ArrayList<>();
-    //     int size = jobs.size();
-    //     System.out.println(size);
-    //     System.out.println(++size);
+    // List<Job> jobs = new ArrayList<>();
+    // int size = jobs.size();
+    // System.out.println(size);
+    // System.out.println(++size);
     // }
 
     @Override
     public void createJob(Job job) {
         job.setId(nextId++);
         jobs.add(job);
-        System.out.println("Job posted "+nextId);
+        System.out.println("Job posted " + nextId);
 
+    }
+
+    @Override
+    public Job getJobById(Long id) {
+        for (Job job : jobs) {
+            if (Objects.equals(job.getId(), id)) {
+                return job;
+            }
+        }
+        return null;
     }
 
 }
