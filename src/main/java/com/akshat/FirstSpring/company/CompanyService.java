03cb0fec7ed979ebccf3a4bd18d3e9728b926480
AkshatRaiShrivastava <shrivastavak2005@gmail.com>
2025-08-16 22:13:04 +0530

    company ka half kaam ho gya

--- src/main/java/com/akshat/FirstSpring/company/CompanyService.java
+++ src/main/java/com/akshat/FirstSpring/company/CompanyService.java
@@ -1,5 +1,7 @@
 package com.akshat.FirstSpring.company;
 
+import java.util.List;
+
 public interface CompanyService {
-    
+    List<Company> getAllCompanies();
 }
--- src/main/java/com/akshat/FirstSpring/company/implementation/CompanyServiceImplementation.java
+++ src/main/java/com/akshat/FirstSpring/company/implementation/CompanyServiceImplementation.java
@@ -1,7 +1,25 @@
 package com.akshat.FirstSpring.company.implementation;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.akshat.FirstSpring.company.Company;
+import com.akshat.FirstSpring.company.CompanyRepository;
 import com.akshat.FirstSpring.company.CompanyService;
 
+
+@Service
 public class CompanyServiceImplementation implements CompanyService{
+    private CompanyRepository companyRepository;
+    
+    public CompanyServiceImplementation(CompanyRepository companyRepository) {
+        this.companyRepository = companyRepository;
+    }
+
+    @Override
+    public List<Company> getAllCompanies() {
+       return companyRepository.findAll();
+    }
     
 }
